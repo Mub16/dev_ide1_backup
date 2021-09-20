e009c6f635f4dd9280efa7784f19deaa4bb02d97
Ivan16 <47645729+Mub16@users.noreply.github.com>
2021-09-20 18:03:22 +0400

    firts_patch
    думаю не сломается не чаго..

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>
+        web
+    </title>
+</head>
+
+<body>
+    <center>
+    <a href="main.php">Users</a>|<a href="Set_Document/index.php">Documents</a>
+    </center>
+    </table>
+</body>
+
+</html>
